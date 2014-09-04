baa78ba990d75ca6f25cb4259112a2abacbe0657
David Bradford <dbradford@omniti.com>
2014-09-04 17:27:27 -0400

    use math random secure and ignore warnings by redirect stderr

--- random.pl
+++ random.pl
@@ -3,12 +3,15 @@
 use 5.14.0;
 use autodie;
 
-#use Math::Random::Secure qw(irand);
+use Math::Random::Secure qw(irand);
+
+local *STDERR;
+open (STDERR, '>', '/dev/null');
 
 # w: 150 x 47
 sub flip {
-    return int(rand(2));
     return irand(2);
+    return int(rand(2));
 }
 
 for my $h (1..47) {
