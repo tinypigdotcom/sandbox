e58d545c5a10cd896f958ff640ed398c6da12fed
David Bradford <dbradford@omniti.com>
2014-09-04 17:27:50 -0400

    first attempt at dnd tool

---
+++ dice.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use 5.14.0;
+use autodie;
+
+use Math::Random::Secure qw(irand);
+
+sub single {
+    return irand(shift)+1;
+}
+
+sub multi {
+    my $input = shift;
+    die unless $input =~ /d/;
+    my @inputs = split /d/, $input;
+    if ( $input =~ /(\d*)d(\d+)(\D?\d*)/ ) {
+        my $num  = $1 || 1;
+        my $die  = $2;
+        my $plus = $3;
+        my $total = 0;
+        for my $count ( 1..$num ) {
+            my $single = single($die);
+            print "die $count: $single\n";
+            $total += $single;
+        }
+        print "... $plus\n";
+        eval "\$total = $total $plus";
+        print "total is $total\n";
+    }
+}
+
+multi(@ARGV);
+
