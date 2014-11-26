1305267f13bd1a0d5fe1796d7f69ed7450202f56
David Bradford <dbradford@omniti.com>
2014-11-26 09:24:42 -0500

    updates

--- getopt.sh
+++ getopt.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 parsed_ops=$(
   perl -MGetopt::Long -le '
+    use strict;
     Getopt::Long::Configure ("bundling");
 
     my @options = (
         "help",
         "version",
         "backdate=i",
+        "bogus=s",
         "yesterday",
     );
 
     # Explicitly add single letter version of each option to allow bundling
-    my ($key, $value);
     my @temp = @options;
     for my $letter (@temp) {
         $letter =~ s/(\w)\w*/$1/;
-        next if $letter eq 'h';
+        next if $letter eq q{h};
         push @options, $letter;
     }
-    # Fix-ups from previous routine
-    push @options, 'h';
+    push @options, q{h};
 
-    Getopt::Long::Configure "bundling";
-    $q = "'\''";
+    my $q = "'\''";
+    use vars qw( $opt_help $opt_version $opt_backdate $opt_bogus $opt_yesterday $opt_v $opt_b $opt_b $opt_y $opt_h );
     GetOptions(@options) or exit 1;
+    my $o;
     for ( map /(\w+)/, @options ) {
         eval "\$o=\$opt_$_";
         $o =~ s/$q/$q\\$q$q/g;
@@ -32,5 +33,6 @@ parsed_ops=$(
 ) || exit
 echo $parsed_ops
 eval "$parsed_ops"
+echo $opt_bogus
 
 
